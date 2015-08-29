4204fe0139995718b244fd521268f77642c3b822
jairomendezmartinez@gmail.com <jairoM26>
2015-08-28 18:38:59 -0600

    Se pone el rapidjson.h para ver si funciona

--- Debug/main.d
+++ Debug/main.d
@@ -1,1 +1,16 @@
-main.d: ../main.cpp
+main.d: ../main.cpp ../DataStructure/LinkedList.h ../DataStructure/Node.h \
+ ../Logic/LogicDucks/Duck.h ../Logic/LogicDucks/../LogicConstants.h \
+ ../Logic/LogicDucks/../LogicDuckMove/Move.h \
+ ../Logic/LogicDucks/../LogicDuckMove/../LogicConstants.h
+
+../DataStructure/LinkedList.h:
+
+../DataStructure/Node.h:
+
+../Logic/LogicDucks/Duck.h:
+
+../Logic/LogicDucks/../LogicConstants.h:
+
+../Logic/LogicDucks/../LogicDuckMove/Move.h:
+
+../Logic/LogicDucks/../LogicDuckMove/../LogicConstants.h:
--- main.cpp
+++ main.cpp
@@ -7,8 +7,16 @@
 
 #include <iostream>
 
+#include "DataStructure/LinkedList.h"
+#include "Logic/LogicDucks/Duck.h"
+
 int main()
 {
+	LinkedList<Duck*>* list_duck = new LinkedList<Duck*>();
+	list_duck->insertData(new Duck(1));
+	list_duck->insertData(new Duck(2));
+	list_duck->insertData(new Duck(3));
+	list_duck->showList();
 	std::cout << "Hola Mundo" << std::endl;
 	return 0;
 }
